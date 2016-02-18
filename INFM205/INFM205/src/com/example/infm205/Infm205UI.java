9a27680c12ffd1c6c7206bc1b2fd781d7748992f
g.lengarski <g.lengarski@gmail.com>
2016-02-18 16:57:05 +0200

    1

--- INFM205/INFM205/src/com/example/infm205/Infm205UI.java
+++ INFM205/INFM205/src/com/example/infm205/Infm205UI.java
@@ -11,9 +11,14 @@ import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalSplitPanel;
@@ -29,6 +34,8 @@ public class Infm205UI extends UI {
 	private HorizontalLayout menuBar;
 	private HorizontalLayout contentLayout;
 	private int menuBarSize = 125;
+	private Button btn;
+	private PopupDateField datetime;
 
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = Infm205UI.class)
@@ -74,7 +81,9 @@ public class Infm205UI extends UI {
 		});
 
 		ComboBox cond = new ComboBox();
+		cond.setNullSelectionAllowed(false);
 		cond.addItems(items2);
+		cond.select(items2.get(0));
 		cond.setNullSelectionAllowed(false);
 		cond.setNewItemsAllowed(false);
 		cond.addValueChangeListener(new ValueChangeListener() {
@@ -85,11 +94,51 @@ public class Infm205UI extends UI {
 			}
 		});
 
+		datetime = new PopupDateField();
+
+		datetime.setValue(new java.util.Date());
+		datetime.setTextFieldEnabled(false);
+		datetime.addValueChangeListener(new ValueChangeListener() {
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				String value = event.getProperty().getValue().toString();
+
+				System.out.println(value);
+			}
+		});
+
+		// Set the correct resolution
+		datetime.setResolution(Resolution.DAY);
+
+		btn = new Button("E");
+		btn.addClickListener(new ClickListener() {
+
+			private boolean b = true;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+
+				b = !b;
+				if (b) {
+					btn.setCaption("E");
+				} else {
+					btn.setCaption("B");
+				}
+
+			}
+		});
 		menuBar.addComponent(new Label("Search: "));
 		menuBar.addComponent(searchBox);
 
 		menuBar.addComponent(new Label("  field 2 "));
 		menuBar.addComponent(cond);
+
+		menuBar.addComponent(new Label("  field 3 "));
+		menuBar.addComponent(datetime);
+
+		menuBar.addComponent(new Label("  field 4 "));
+		menuBar.addComponent(btn);
 
 		mainLayout.setFirstComponent(menuBar);
 
